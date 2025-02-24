27370806ba8d9770afb091a6a9515dfdf3986223
Berke Ates <berke@ates.ch>
2025-02-24 13:51:06 +0100

    better compile script

--- compile.py
+++ compile.py
@@ -2,10 +2,6 @@ import dace
 import shutil
 import os
 
-from dace.transformation.interstate import LoopToMap
-from dace.transformation.passes.struct_to_container_group import StructToContainerGroups
-
-
 # Mapping of SDFG -> headerfile
 header_dict = {
     "add_aerosol_optics": "serdeae.h",
@@ -13,7 +9,7 @@ header_dict = {
     "cloud_optics_fn_438": "serdeco.h",
     "crop_cloud_fraction": "serdeccf.h",
     "gas_optics": "serdego.h",
-    "get_albedos": None,
+    "get_albedos": "serde_get_albedos.h",
     "solver_mcica_lw": "serdesl.h",
     "solver_mcica_sw": "serdess.h",
 }
@@ -31,81 +27,60 @@ main_dict = {
 }
 
 
-# load the sdfg
-# sdfg = dace.SDFG.from_file("sdfgs/add_aerosol_optics_simplified_dbg22.sdfgz") # ++ (segfault after S2CG)
-# sdfg = dace.SDFG.from_file("sdfgs/calc_surface_spectral_simplified_dbg22.sdfgz") # x
-# sdfg = dace.SDFG.from_file("sdfgs/cloud_optics_fn_438_simplified_dbg22.sdfgz") # failed assertion
-sdfg = dace.SDFG.from_file("sdfgs/crop_cloud_fraction_simplified_dbg22.sdfgz") # +++
-# sdfg = dace.SDFG.from_file("sdfgs/gas_optics_simplified_dbg22.sdfgz") # x
-# sdfg = dace.SDFG.from_file("sdfgs/get_albedos_simplified_dbg22.sdfgz") # x
-# sdfg = dace.SDFG.from_file("sdfgs/solver_mcica_lw_simplified_dbg22.sdfgz") # unsorted double linked list corrupted
-# sdfg = dace.SDFG.from_file("sdfgs/solver_mcica_sw_simplified_dbg22.sdfgz") # bad array new length
+# Choose the SDFG to run
+path = "sdfgs/add_aerosol_optics_simplified_dbg22.sdfgz"
+# path = "sdfgs/calc_surface_spectral_simplified_dbg22.sdfgz"
+# path = "sdfgs/cloud_optics_fn_438_simplified_dbg22.sdfgz"
+# path = "sdfgs/crop_cloud_fraction_simplified_dbg22.sdfgz"
+# path = "sdfgs/gas_optics_simplified_dbg22.sdfgz"
+# path = "sdfgs/get_albedos_simplified_dbg22.sdfgz"
+# path = "sdfgs/solver_mcica_lw_simplified_dbg22.sdfgz"
+# path = "sdfgs/solver_mcica_sw_simplified_dbg22.sdfgz"
 
+# Load SDFG
+sdfg = dace.SDFG.from_file(path)
 
-# # How many loops before?
-# loop_cnt = 0
-# for node, state in sdfg.all_nodes_recursive():
-#     if isinstance(state, dace.sdfg.state.LoopRegion):
-#         loop_cnt += 1
-
-# print(f"loops BEFORE loop2map: {loop_cnt}")
-
-# # apply loop2map transformation
-# # num_apps=sdfg.apply_transformations_repeated(LoopToMap)
-
-# num_apps = StructToContainerGroups().apply_pass(sdfg, {})
-
-
-# print(f"Applied {num_apps} LoopToMap transformations")
-
-# # How many loops after?
-# loop_cnt = 0
-# for node, state in sdfg.all_nodes_recursive():
-#     if isinstance(state, dace.sdfg.state.LoopRegion):
-#         loop_cnt += 1
-
-# print(f"loops AFTER loop2map: {loop_cnt}")
-
-# sdfg.save("containergrouped.sdfg")
-
-# exit(0)
 
 ################################################################################
-# Run the altered SDFG
+### Apply Optimizations
+################################################################################
 
-# compile the sdfg
+# TODO: Add Optimizations here for each SDFG
+
+
+################################################################################
+### Compile the (optimized) SDFG with alterations
+################################################################################
+
+# compile the SDFG
 sdfg.compile()
-
 
 # get build location and dace location
 build_loc = sdfg.build_folder
 sdfg_name = sdfg.name
 dace_include = os.path.dirname(dace.__file__) + "/runtime/include/"
 
-# copy main_cpp_files/main_solver_mcica_lw.cc to .dacecache/solver_mcica_lw/src/cpu/main_solver_mcica_lw.cc
+# copy main_cpp_file to .dacecache/<name>/src/cpu/
 main_name = main_dict[sdfg_name]
-shutil.copy(
-    f"main_cpp_files/{main_name}", f"{build_loc}/src/cpu/{main_name}"
-)
+shutil.copy(f"main_cpp_files/{main_name}", f"{build_loc}/src/cpu/{main_name}")
 
 # change the line "constexpr char ROOT[] ="
-# to contain "/home/xdb/icon-artifacts/inputs/solver_mcica_lw/"
+# to contain "<path to this script>/inputs/<name>/"
+input_folder = os.path.dirname(os.path.realpath(__file__)) + "/inputs"
 with open(f"{build_loc}/src/cpu/{main_name}", "r") as file:
     lines = file.readlines()
 with open(f"{build_loc}/src/cpu/{main_name}", "w") as file:
     for line in lines:
         if line.startswith("constexpr char ROOT[] ="):
-            file.write(
-                f'constexpr char ROOT[] = "/home/xdb/icon-artifacts/inputs/{sdfg_name}/";\n'
-            )
+            file.write(f'constexpr char ROOT[] = "{input_folder}/{sdfg_name}/";\n')
         else:
             file.write(line)
 
-# copy headers/serdesl.h to .dacecache/solver_mcica_lw/include/serdesl.h
+# copy header to .dacecache/<name>/include/
 header_name = header_dict[sdfg_name]
 shutil.copy(f"headers/{header_name}", f"{build_loc}/include/{header_name}")
 
-# compile c++ solver_mcica_lw.cpp main_solver_mcica_lw.cc -I../../include -I/<pathtodace>/dace/runtime/include/ -std=c++17 -O0 -ggdb
+# compile c++ <SDFG cpp file> <main file> -I../../include -I/<pathtodace>/dace/runtime/include/ -std=c++17 -O0 -ggdb
 os.system(
     f"c++ {build_loc}/src/cpu/{sdfg_name}.cpp {build_loc}/src/cpu/{main_name} -I {build_loc}/include -I {dace_include} -std=c++17 -O0 -ggdb -o {sdfg_name}"
 )
